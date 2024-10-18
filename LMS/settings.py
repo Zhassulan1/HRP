1cb137169ab3970644a02540147ab9fdc50ca2ba
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-10-18 16:37:36 +0500

    fix: added dotenv loading

--- LMS/settings.py
+++ LMS/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+print("Secret key:", SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
