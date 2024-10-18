70bbb11178e5b97f33f33beca7b3f7c3c696c592
Zhassulan Kainazarov <zhkainazarov@gmail.com>
2024-10-18 16:39:21 +0500

    added dotenv load example

--- LMS/settings.py
+++ LMS/settings.py
@@ -150,3 +150,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Other secret keys
+# Example
+# OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
